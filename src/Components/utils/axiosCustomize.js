5323e10c0a7656bc36a31127e5cb06baceb5b84c
Đệ giáo-làng <truongvshieu01@gmail.com>
2023-10-15 16:36:41 +0700

    second upload

---
+++ src/Components/utils/axiosCustomize.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+const instance = axios.create({
+  baseURL: "http://localhost:8080/",
+});
+
+// Add a request interceptor
+instance.interceptors.request.use(
+  function (config) {
+    // Do something before request is sent
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+//interceptor : middleware
+instance.interceptors.response.use(
+  //only get response.data
+  function (response) {
+    console.log(">>> interceptor", response);
+    return response && response.data ? response.data : response;
+  },
+  function (error) {
+    // console.log(">>> run error: ", error.response);
+    return error && error.response & error.response.data
+      ? error.response.data
+      : Promise.reject(error);
+  }
+);
+
+export default instance;
