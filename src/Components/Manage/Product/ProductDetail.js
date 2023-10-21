77ae5b3685880ce8df4f15f576993e1f87dd0906
tohka-vo-tri <imchibitouka@gmail.com>
2023-10-21 14:37:44 +0700

    commit 21_10 Feedback product

--- src/Components/Manage/Product/ProductDetail.js
+++ src/Components/Manage/Product/ProductDetail.js
@@ -3,12 +3,16 @@
 import axios from "axios";
 import '/node_modules/primeflex/primeflex.css';
 import authHeader from "../../AuthHeader/AuthHeader";
-
+import { FilterMatchMode, FilterOperator } from 'primereact/api';
 import { InputTextarea } from 'primereact/inputtextarea';
 import React, { useState, useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
-
+import { Button } from 'primereact/button';
 import { InputNumber } from 'primereact/inputnumber';
+import { Toast } from 'primereact/toast';
+import { InputText } from "primereact/inputtext";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
 export default function ProductDetail() {
 
     const { productId } = useParams();
@@ -22,6 +26,7 @@ export default function ProductDetail() {
     const [isEditingImg, setIsEditingImg] = useState(false);
     const [showSaveButton, setShowSaveButton] = useState(false);
     const [selectedFile, setSelectedFile] = useState(null);
+    const [selectedCustomer3, setSelectedCustomer3] = useState(null);
 
     const defaultImageUrl = "http://localhost:3000/img/default.jpg";
 
@@ -65,19 +70,18 @@ export default function ProductDetail() {
             });
     }, []);
 
-    const [feedback, setFeedback] = useState({});
-
+    const [feedback, setFeedback] = useState([]);
+    const toast = useRef(null);
     useEffect(() => {
-
         axios.get(`http://localhost:8080/zoo-server/api/v1/feedback/getFeedbackByProduct${productId}`, { headers: authHeader() })
             .then((response) => {
-
-                setFeedback(response.data.data);
+                setFeedback(response.data.data); // Make sure response.data.data is an array
             })
             .catch((error) => {
                 console.error('Error fetching user profile:', error);
             });
     }, []);
+
 
 
     const handleEditClick = () => {
@@ -147,7 +151,7 @@ export default function ProductDetail() {
                 locale="en-US"
             />
         ) : (
-            <span>{editedProduct[field] || defaultValue}</span>
+            <span>{editedProduct[field] || defaultValue}$</span>
         );
     };
 
@@ -156,6 +160,27 @@ export default function ProductDetail() {
         return n !== Infinity && String(n) === String(val) && n > 0;
     };
 
+    const deleteFeedback = (feedbacks) => {
+        axios
+            .delete(`http://localhost:8080/zoo-server/api/v1/feedback/deleteFeedback/${feedbacks.feedbackId}`, { headers: authHeader() })
+            .then((response) => {
+                // Xóa thành công, cập nhật danh sách sản phẩm ở phía client
+                console.log('check feedback id', feedback.feedbackId);
+                const updatedFeedback = feedback.filter((item) => item.feedbackId !== feedbacks.feedbackId);
+                setFeedback(updatedFeedback);
+
+                alert(response.data.message);
+            })
+            .catch((error) => {
+                console.error('Lỗi khi xóa sản phẩm:', error);
+            });
+    };
+    const show = (message, color) => {
+        toast.current.show({
+            summary: 'Notifications', detail: message, life: 3000,
+            style: { backgroundColor: color, color: 'white', border: '2px solid yellow' },
+        });
+    };
 
 
 
@@ -172,12 +197,33 @@ export default function ProductDetail() {
                 }}
             />
         ) : (
-            <span>{editedProduct[field] || defaultValue}</span>
+            <span>{editedProduct[field] || defaultValue} </span>
         );
     };
 
+    /*<label htmlFor="updateDescription">Description</label>
+                  <br />
+                  <InputTextarea
+                    id="description"
+                    name="description"
+                    value={newProduct.description}
+                    onChange={handleInputChange}
+                  /> */
 
-
+    const displayWithDefaultDescription = (field, defaultValue = 'Not Provided') => {
+        return isEditing ? (
+            <InputTextarea
+                className="text-base text-color surface-overlay p-2 border-1 border-solid surface-border border-round appearance-none outline-none focus:border-primary w-8"
+                name={field}
+                type="text"
+                placeholder={`Enter your ${field}`}
+                value={editedProduct[field] || ''}
+                onChange={handleInputChange}
+            />
+        ) : (
+            <span>{editedProduct[field] || defaultValue}</span>
+        );
+    };
 
     const displayWithDefault = (field, defaultValue = 'Not Provided') => {
         return isEditing ? (
@@ -209,24 +255,85 @@ export default function ProductDetail() {
         );
 
     };
+    const actionBodyTemplate = (rowData) => {
+        return (
+            <div>
+
+                <Button
+                    icon="pi pi-trash"
+                    className="p-button-danger"
+                    onClick={() => deleteFeedback(rowData)}
+                />
+                <Button
+                    icon="pi pi-eye"
+                    className="p-button-info"
+                    onClick={() => window.location.href = `user-details/${rowData.userId}`}
+                />
+            </div>
+        );
+    };
+
+
+
+
+    const renderHeader = (filtersKey) => {
+        const filters = filtersMap[`${filtersKey}`].value;
+        const value = filters['global'] ? filters['global'].value : '';
+
+        return (
+            <div class="grid">
+                <div class="col-3">
+                    <span className="p-input-icon-left">
+                        <i className="pi pi-search" />
+                        <InputText type="search" value={value || ''} onChange={(e) => onGlobalFilterChange(e, filtersKey)} placeholder="Feedback Search" />
+                    </span>
+                </div>
+
+
+            </div>
+        );
+    }
+    const [filters, setFilters] = useState({
+        'global': { value: null, matchMode: FilterMatchMode.CONTAINS },
+        'content': { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] },
+    });
+
+
+    const filtersMap = {
+
+        'filters': { value: filters, callback: setFilters },
+    };
+
+
+    const onGlobalFilterChange = (event, filtersKey) => {
+        const value = event.target.value;
+        let filters = { ...filtersMap[filtersKey].value };
+        filters['global'].value = value;
+
+        filtersMap[filtersKey].callback(filters);
+    }
+
+    const header3 = renderHeader('filters');
+    const onCustomSaveState = (state) => {
+        sessionStorage.setItem('dt-state-demo-custom', JSON.stringify(state));
+    }
+    const onCustomRestoreState = () => {
+        return JSON.parse(sessionStorage.getItem('dt-state-demo-custom'));
+    }
 
     return (
 
 
         <div className="container-xl px-4 mt-4">
-
-            <hr className="mt-0 mb-4" />
             <div className="grid">
-                <div className="col-6">
-                    <div className="w-full">
-                        <div className="card ">
+                <div className="col-12">
+                    <div className="row">
+                        <div className="card col-5">
                             <div className="card-header">Product Image</div>
                             <div className="card-body text-center">
                                 <div>
                                     <img alt="Card" style={{ width: '200px', height: '200px' }} src={`http://localhost:3000/img/${editedProduct.image}`} />
-                                    <br />
-                                    <label htmlFor="updateAnimalImage">Image</label>
-                                    <br />
+
                                     {displayWithAvatar('image')}
                                 </div>
 
@@ -235,6 +342,10 @@ export default function ProductDetail() {
                             <div className="small font-italic text-muted mb-4">
                                 {editedProduct.productName ? `${editedProduct.productName} ` : 'Name Not Provided'}
                             </div>
+
+                        </div>
+                        <div className="col-1"></div>
+                        <div className="card col-6">
                             <div className="card-header">Information Product</div>
                             <div className="card-body">
                                 <form>
@@ -248,7 +359,7 @@ export default function ProductDetail() {
                                         <label className="mb-1 mr-2 font-bold" htmlFor="inputDescription">
                                             Descriptione:
                                         </label>
-                                        {displayWithDefault('description')}
+                                        {displayWithDefaultDescription('description')}
                                     </div>
                                     <div className="field flex">
                                         <label className="mb-1 mr-2 font-bold" htmlFor="inputPrice">
@@ -299,17 +410,32 @@ export default function ProductDetail() {
                         </div>
                     </div>
                 </div>
-                <div className="col-6">
-                    <div className="card mb-4">
+                <div className="col-12">
 
-                    </div>
-                    <div className="card-header">Report Product</div>
+
+
                     <div className="card-body">
+                        <div className="datatable-editing-demo">
+                            <Toast ref={toast} />
+                            <div className="card p-fluid">
+                                <div className="card-header">Feedback Product</div>
+
+                                <DataTable value={feedback} paginator rows={5} header={header3} filters={filters} onFilter={(e) => setFilters(e.filters)}
+                                    selection={selectedCustomer3} onSelectionChange={e => setSelectedCustomer3(e.value)} selectionMode="single" dataKey="id" responsiveLayout="scroll"
+                                    stateStorage="custom" customSaveState={onCustomSaveState} customRestoreState={onCustomRestoreState} emptyMessage="Not found.">
+                                    <Column field="feedbackId" header="ID" style={{ width: '10%' }}></Column>
+                                    <Column field="fullName" header="Customer Name" sortable style={{ width: '20%', textAlign: 'center' }}></Column>
+                                    <Column field="content" header="Content" sortable style={{ width: '30%' }}></Column>
+                                    {/* <Column field="productName" header="Product Name" sortable style={{ width: '15%' }}></Column> */}
+                                    <Column field="rating" header="Rating" sortable style={{ width: '20%' }}></Column>
+                                    <Column field="Actions" header="Actions" style={{ width: '15%' }} body={actionBodyTemplate}></Column>
+                                </DataTable>
+                            </div>
+                        </div>
 
                     </div>
 
                 </div>
-
             </div >
         </div >
     );
