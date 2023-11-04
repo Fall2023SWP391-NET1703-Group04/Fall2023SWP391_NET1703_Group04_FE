ce09b8444085bd8d3a340210174c02425b915a18
Đệ giáo-làng <truongvshieu01@gmail.com>
2023-11-04 09:57:35 +0700

    register

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -11,36 +11,111 @@ import { Divider } from "primereact/divider";
 import { classNames } from "primereact/utils";
 import "./Register.css";
 import { Link } from "react-router-dom";
-
+import axios from "axios";
+import authHeader from "../AuthHeader/AuthHeader";
 const Register = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    phoneNumber: "",
+    email: "",
+    password: "",
+  });
+  const [alertMessage, setAlertMessage] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/zoo-server/api/v1/auth/signIn",
+        formData,
+        {
+          headers: authHeader(),
+        }
+      );
+      console.log(response);
+
+      if (response.status === 200) {
+        // Registration successful
+        const token = response.data.token;
+        // You can save the JWT token in localStorage or a secure storage mechanism
+        localStorage.setItem("jwtToken", token);
+
+        setAlertMessage("Registration successful!");
+      } else {
+        setAlertMessage("Registration failed.");
+      }
+    } catch (error) {
+      setAlertMessage("Registration failed.");
+    }
+  };
   return (
     <>
       <div className="wrapper-register">
         <div className="box">
           <span className="borderLine"></span>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="inputBox">
-              <input type="text" required="required" />
+              <input
+                type="text"
+                name="firstName"
+                value={formData.firstName}
+                onChange={handleChange}
+                required
+              />
               <span>FirstName</span>
               <i></i>
             </div>
             <div className="inputBox">
-              <input type="text" required="required" />
+              <input
+                type="text"
+                name="lastName"
+                value={formData.lastName}
+                onChange={handleChange}
+                required
+              />
               <span>LastName</span>
               <i></i>
             </div>
             <div className="inputBox">
-              <input type="text" required="required" />
+              <input
+                type="text"
+                name="phoneNumber"
+                value={formData.phoneNumber}
+                onChange={handleChange}
+                required
+              />
               <span>Phone Number</span>
               <i></i>
             </div>
             <div className="inputBox">
-              <input type="email" required="required" />
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
               <span>Email</span>
               <i></i>
             </div>
             <div className="inputBox">
-              <input type="password" required="required" />
+              <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                required
+              />
               <span>Password</span>
               <i></i>
             </div>
@@ -52,6 +127,7 @@ const Register = () => {
           </form>
         </div>
       </div>
+      {alertMessage && <div className="alert">{alertMessage}</div>}
     </>
   );
 };
